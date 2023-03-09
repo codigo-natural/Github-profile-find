cb8d8aa58a9d3139670b412a44065e89c0e79561
Code-Nature <codenature01001@gmail.com>
2023-03-09 11:56:05 -0500

    creando el contenedor y el text field

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container } from '@mui/material'
+import Searcher from './components/Searcher';
+
+const App = () => {
+  return (
+    <Container sx={{
+      background: '#3fc',
+      width: '80vw',
+      height: '500px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }}>
+      <Searcher />
+    </Container>
+  )
+}
+
+export default App;
---
+++ src/components/Searcher/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Stack } from '@mui/system'
+import { IconButton, TextField } from '@mui/material'
+
+const Searcher = () => {
+  return (
+
+    <Stack>
+      <TextField>
+      </TextField>
+      <IconButton></IconButton>
+    </Stack>
+  )
+}
+
+export default Searcher

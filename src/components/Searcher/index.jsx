3da5823a6e8146528878c8952f83a64303d4523b
Code-Nature <codenature01001@gmail.com>
2023-03-11 09:33:00 -0500

    se añadio Fragment al componente Searcher

--- src/components/Searcher/index.jsx
+++ src/components/Searcher/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import { Stack } from '@mui/system'
 import { IconButton, TextField, Typography } from '@mui/material'
 import SearchIcon from "@mui/icons-material/Search";
@@ -25,33 +25,35 @@ const Searcher = ({ setInputUser, notFound }) => {
   }
 
   return (
-    <><Stack direction='row' sx={estilos}>
-      <TextField
-        sx={styleTextField}
-        type='search'
-        autoComplete='off'
-        label='GitHub User'
-        size='small'
-        value={valueInput}
-        onChange={onSearchValueChange}
-        id='outline-basic'
-        variant='outlined'
-        placeholder='octocat'
-        InputProps={{
-          endAdornment: (
-            <IconButton
-              size='small'
-              onClick={handleSubmit}
-            >
-              <SearchIcon />
-            </IconButton>
-          )
-        }} />
-    </Stack><Typography color='red'>
+    <Fragment>
+      <Stack direction='row' sx={estilos}>
+        <TextField
+          sx={styleTextField}
+          type='search'
+          autoComplete='off'
+          label='GitHub User'
+          size='small'
+          value={valueInput}
+          onChange={onSearchValueChange}
+          id='outline-basic'
+          variant='outlined'
+          placeholder='octocat'
+          InputProps={{
+            endAdornment: (
+              <IconButton
+                size='small'
+                onClick={handleSubmit}
+              >
+                <SearchIcon />
+              </IconButton>
+            )
+          }} />
+      </Stack><Typography color='red'>
         {notFound
           ? 'Error: usuario no existe'
           : ''}
-      </Typography></>
+      </Typography>
+    </Fragment>
   )
 }
 
